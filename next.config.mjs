c06bd97ce0057ee7337d9b687badac11e120f925
Jide Adedeji <jideadedejifirst@gmail.com>
2024-12-05 15:02:36 -0500

    added service worker

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
+// @ts-check
+import withSerwistInit from '@serwist/next';
+import { nanoid } from 'nanoid/non-secure';
+
+// You may want to use a more robust revision to cache
+// files more efficiently.
+// A viable option is `git rev-parse HEAD`.
+let revision = nanoid();
+
+const withSerwist = withSerwistInit({
+  cacheOnNavigation: true,
+  swSrc: 'sw.ts',
+  swDest: 'public/sw.js',
+  additionalPrecacheEntries: [{ url: '/~offline', revision }],
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
